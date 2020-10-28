e6b8a21cd31638452b386ce48c3bffc61d57169f
PabloAtayde <71047880+PabloAtayde@users.noreply.github.com>
2020-10-27 19:37:41 -0600

    funciones1.0

--- app/src/main/java/com/example/calculadora_proyecto1/MainActivity.java
+++ app/src/main/java/com/example/calculadora_proyecto1/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.calculadora_proyecto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnp, btnsum, btnres, btnmul, btndiv,
+            btnig, btnclr,btnborr,btnce;
+    TextView label;
+    String res, n1, n2, ide;
+    Double doblee;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
